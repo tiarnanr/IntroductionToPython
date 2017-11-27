2aef2552dd5c2c658ad83e253f87a165de440452
ricet <ricet@rose-hulman.edu>
2017-11-27 16:42:49 -0500

    Completed m5_your_turtles.py

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Tiarnan Rice.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,46 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+purple_turtle = rg.SimpleTurtle('turtle')
+purple_turtle.pen = rg.Pen('purple', 20)
+purple_turtle.speed = 20
+purple_turtle.pen_up()
+
+purple_turtle.left(90)
+purple_turtle.forward(300)
+purple_turtle.left(90)
+purple_turtle.forward(300)
+purple_turtle.right(180)
+
+for k in range(10):
+    purple_turtle.forward(600)
+    purple_turtle.right(180)
+    purple_turtle.pen_up()
+    purple_turtle.forward(600)
+    purple_turtle.left(90)
+    purple_turtle.forward(80)
+    purple_turtle.left(90)
+    purple_turtle.pen_down()
+
+red_turtle = rg.SimpleTurtle('turtle')
+red_turtle.pen = rg.Pen('red', 3)
+red_turtle.speed = 20
+red_turtle.left(90)
+red_turtle.pen_up()
+red_turtle.forward(200)
+red_turtle.left(90)
+red_turtle.forward(250)
+red_turtle.right(180)
+red_turtle.pen_down()
+
+for i in range(72):
+    red_turtle.draw_circle(30)
+    red_turtle.forward(20)
+    red_turtle.right(i / 5)
+
+window.close_on_mouse_click()
